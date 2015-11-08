ab99011568f467a4c6c069494b5a9579c7e186bd
Son Linh <linh.nguyenson@gmail.com>
2015-11-08 19:16:02 +0700

    đội ngữ giáo viên

--- wp-config.php
+++ wp-config.php
@@ -16,16 +16,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'wp');
+define('DB_NAME', 'mosaweb_dev15');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'mosaweb_dev15');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'hibernate1');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', '192.254.234.193');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
