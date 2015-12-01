2943df6b62e7af452329e96844f89a72ac8ca829
Son Linh <linh.nguyenson@gmail.com>
2015-12-02 00:22:00 +0700

    fix blog

--- wp-content/themes/thememe/inc/extras.php
+++ wp-content/themes/thememe/inc/extras.php
@@ -198,3 +198,8 @@ function wpbeginner_numeric_posts_nav() {
 	echo '</ul></nav>' . "\n";
 
 }
+
+function new_excerpt_more( $more ) {
+	return ' <a class="more-link" href="' . get_permalink( get_the_ID() ) . '">' . __( 'Xem thêm', 'your-text-domain' ) . '</a>';
+}
+add_filter( 'excerpt_more', 'new_excerpt_more' );
